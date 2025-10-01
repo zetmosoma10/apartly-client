48b3b605596713990a8e6d82593a5906233ea194
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-10-01 16:33:26 +0200

    configure react router

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 import { RiBuilding2Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="navbar bg-base-100 justify-between ">
       <div className="flex items-center space-x-3">
         <RiBuilding2Line size="20px" className="text-warning" />
-        <a href="#" className="logo-font font-bold text-xl">
+        <Link to="/" className="logo-font font-bold text-xl">
           Apartly
-        </a>
+        </Link>
       </div>
       <div>Listings</div>
       <div>profile</div>
