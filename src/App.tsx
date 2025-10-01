a022a5dcd588e755d24582a48064da41f441d912
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-10-01 15:04:25 +0200

    add inter google font

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ function App() {
   return (
     <section>
       <main>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+          molestias. Ipsam, ad numquam. Doloremque, neque porro. Molestias quos
+          vitae nihil?
+        </p>
         <button className="btn btn-primary">submit</button>
       </main>
     </section>
