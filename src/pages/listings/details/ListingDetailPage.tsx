e89473410a30cd54b3318f6be2ff3196c070a3f4
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-10-07 09:34:45 +0200

    Fix: Layout issue on image grid on apartment

--- src/pages/listings/details/ListingDetailPage.tsx
+++ src/pages/listings/details/ListingDetailPage.tsx
@@ -8,61 +8,69 @@ const ListingDetailPage = () => {
   const { data, isLoading } = useGetApartment(id);
   const apartment = data?.results;
 
-  if(isLoading) return <p>Loading...</p>
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <section className="w-full mx-auto md:max-w-5xl">
       <h2>{apartment?.title}</h2>
-      <p className="text-sm md:text-base opacity-70">
+      <p className="text-base md:text-lg opacity-70">
         {apartment?.address}, {apartment?.city}
       </p>
       <Badge status={apartment!.status} />
-      <div className="grid sm:grid-cols-6 gap-3 mt-4 md:mt-5 ">
-        <div className="col-span-4">
-          <img
-            src={apartment?.images[0].url}
-            alt="apartment image"
-            className="h-full object-cover rounded-lg "
-          />
-        </div>
-        <div className="col-span-2  grid gap-3">
-          <img
-            src={apartment?.images[1].url}
-            alt="apartment image"
-            className="object-cover rounded-lg "
-          />
-          <img
-            src={apartment?.images[2].url}
-            alt="apartment image"
-            className="object-cover rounded-lg"
-          />
-        </div>
+      <div className="grid grid-cols-3 grid-rows-2 gap-4 h-[500px] mt-8 mb-12">
+        <div
+          className="col-span-2 row-span-2 overflow-hidden rounded-xl"
+          style={{
+            backgroundImage: `url(${apartment?.images[0].url})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div
+          className="col-span-1 row-span-1 overflow-hidden rounded-xl"
+          style={{
+            backgroundImage: `url(${apartment?.images[1].url})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div
+          className="col-span-1 row-span-1 overflow-hidden rounded-xl"
+          style={{
+            backgroundImage: `url(${apartment?.images[2].url})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
       </div>
-      <div className="flex flex-cols sm:flex-row sm:justify-between mt-5">
+      <div className="flex mt-5 flex-cols sm:flex-row sm:justify-between">
         <div>
           <h3>Descriptions</h3>
-          <p className="opacity-70 leading-tight">{apartment?.description}</p>
+          <p className="leading-tight opacity-70">{apartment?.description}</p>
         </div>
         <div className="border p-4 min-w-[300px] bg-white shadow-md rounded-lg">
-          <h4 className="text-base">R {apartment.price} / month</h4>
-          <p className="text-xs opacity-70 mb-2">
-            <span>{apartment.bedrooms} bedroom(s) . {apartment.bathrooms} bathroom(s)</span>
+          <h4 className="">R {apartment?.price} / month</h4>
+          <p className="mb-3 text-xs opacity-70">
+            <span>
+              {apartment?.bedrooms} bedroom(s) • {apartment?.bathrooms}{" "}
+              bathroom(s)
+            </span>
           </p>
           <Link
-            to="/landlord" 
-            className="btn btn-sm btn-outline btn-warning w-full rounded-2xl"
+            to="/landlord"
+            className="w-full btn btn-sm btn-outline btn-warning rounded-2xl"
           >
-          Contact Landlord
+            Contact Landlord
           </Link>
         </div>
       </div>
       <div className="mt-6">
-        <h4>Amenites</h4>
-        <ul className="flex items-center flex-wrap gap-3 mt-3">
+        <h3>Amenites</h3>
+        <ul className="flex flex-wrap items-center gap-3 mt-3">
           {apartment?.amenities.map((item, index) => (
             <li
               key={index}
-              className="text-sm p-2 px-3 border border-warning rounded-lg text-warning-content"
+              className="p-2 px-3 text-sm border rounded-lg border-warning text-warning-content"
             >
               {item}
             </li>
