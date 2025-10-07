1e22e0b3a34c86af8b5b92ca2a44de4f2194db08
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-10-07 12:20:16 +0200

    allow apartment form to handle both update and create apartment logic

--- src/pages/listings/ApartmentForm.tsx
+++ src/pages/listings/ApartmentForm.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { apartmentSchema } from "./validationSchemas";
 import { z } from "zod";
+import type { Apartment } from "../../entities/Apartment";
 import Input from "../../components/Input";
 import SelectInput from "./new/SelectInput";
 import UploadBox from "./new/UploadBox";
@@ -11,13 +13,33 @@ import useCreateApartment from "../../hooks/useCreateApartment";
 
 type FormData = z.infer<typeof apartmentSchema>;
 
-const ApartmentForm = () => {
+const ApartmentForm = ({ apartment }: { apartment?: Apartment }) => {
+  const { id } = useParams();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>({
     resolver: zodResolver(apartmentSchema),
+    defaultValues: id
+      ? {
+          title: apartment?.title ?? "",
+          description: apartment?.description ?? "",
+          address: apartment?.address ?? "",
+          city: apartment?.city ?? "",
+          price: apartment?.price ? Number(apartment.price) : undefined,
+          bedrooms: apartment?.bedrooms
+            ? Number(apartment.bedrooms)
+            : undefined,
+          bathrooms: apartment?.bathrooms
+            ? Number(apartment.bathrooms)
+            : undefined,
+          status: apartment?.status ?? "available",
+          amenities: apartment?.amenities ? apartment.amenities.join(", ") : "",
+          type: apartment?.type ?? "other",
+        }
+      : undefined,
   });
 
   // * file + image preview state
@@ -91,21 +113,21 @@ const ApartmentForm = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="border rounded-xl mt-6 p-6 bg-base-100 shadow-md"
+      className="p-6 mt-6 border shadow-md rounded-xl bg-base-100"
     >
       <div className="space-y-3">
         <Input
           id="title"
           label="Title"
-          placeholder="e.g 2-bedrooms"
+          placeholder="e.g 2-bedrooms apartment with mountain view"
           register={register("title")}
           error={errors.title?.message}
         />
-        <div className="grid sm:grid-cols-2 gap-3">
+        <div className="grid gap-3 sm:grid-cols-2">
           <Input
             id="price"
             label="Price"
-            placeholder="e.g R 4500"
+            placeholder="e.g 4500"
             register={register("price")}
             error={errors.price?.message}
           />
@@ -117,7 +139,7 @@ const ApartmentForm = () => {
             error={errors.bedrooms?.message}
           />
         </div>
-        <div className="grid sm:grid-cols-2 gap-3">
+        <div className="grid gap-3 sm:grid-cols-2">
           <Input
             id="city"
             label="City"
@@ -149,7 +171,7 @@ const ApartmentForm = () => {
             error={errors.address?.message}
           />
         </div>
-        <div className="grid sm:grid-cols-2 gap-3">
+        <div className="grid gap-3 sm:grid-cols-2">
           <Input
             id="bathrooms"
             label="Bathrooms"
@@ -180,31 +202,33 @@ const ApartmentForm = () => {
           register={register("description")}
           error={errors.description?.message}
         />
-        <UploadBox onImageFileChange={onImageFileChange} />
+        {!id && <UploadBox onImageFileChange={onImageFileChange} />}
       </div>
 
       {/* Image previews */}
-      <div>
-        {previews.length > 0 && (
-          <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 mt-4">
-            {previews.map((src, i) => (
-              <div key={i} className="relative">
-                <img
-                  src={src}
-                  alt={`preview-${i}`}
-                  className="w-full h-32 object-cover rounded-lg"
-                />
-                <span className="absolute top-1 right-1 bg-black bg-opacity-50 text-white text-xs px-1 rounded">
-                  {i + 1}
-                </span>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      {!id && (
+        <div>
+          {previews.length > 0 && (
+            <div className="grid grid-cols-2 gap-3 mt-4 sm:grid-cols-3">
+              {previews.map((src, i) => (
+                <div key={i} className="relative">
+                  <img
+                    src={src}
+                    alt={`preview-${i}`}
+                    className="object-cover w-full h-32 rounded-lg"
+                  />
+                  <span className="absolute px-1 text-xs text-white bg-black bg-opacity-50 rounded top-1 right-1">
+                    {i + 1}
+                  </span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
 
-      <button disabled={isPending} className="btn btn-warning mt-5">
-        Submit New Apartment
+      <button disabled={isPending} className="mt-5 btn btn-warning">
+        {id ? "Update Apartment" : "Submit New Apartment"}
         {isPending && (
           <span className="loading loading-spinner loading-sm text-warning"></span>
         )}
--- src/pages/listings/edit/ListingEditPage.tsx
+++ src/pages/listings/edit/ListingEditPage.tsx
@@ -1,5 +1,26 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import type { Response } from "../../../entities/Response";
+import type { Apartment } from "../../../entities/Apartment";
+import ApartmentForm from "../ApartmentForm";
+
 const ListingEditPage = () => {
-  return <div>ListingEditPage</div>;
+  const { id } = useParams();
+  const queryClient = useQueryClient();
+  const query = queryClient.getQueryData<Response<Apartment>>([
+    "apartment",
+    id,
+  ]);
+
+  const apartment = query?.results;
+  return (
+    <section className="w-full mx-auto md:max-w-5xl">
+      <h2>Edit Apartment: {apartment?.title}</h2>
+      <div>
+        <ApartmentForm apartment={apartment} />
+      </div>
+    </section>
+  );
 };
 
 export default ListingEditPage;
