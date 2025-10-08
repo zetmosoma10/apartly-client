dc8c5aa1cc07f0febec7901f24fcf535bda6c336
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-10-08 09:14:18 +0200

    build auth page

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  return (
+    <section className="flex items-center justify-center h-screen">
+      <div className="text-center max-w-[430px]">
+        <h1 className="font-extrabold text-5xl sm:text-6xl">
+          Find Your <span className="text-warning">Next</span> Home
+        </h1>
+        <p className="opacity-70 mt-3">
+          Choose your role to sign in or create an account.
+        </p>
+        <div className="flex flex-col gap-3 mt-4">
+          <Link
+            to="register"
+            className="btn btn-outline hover:bg-warning hover:text-white rounded-3xl"
+          >
+            I'm a Tenant
+          </Link>
+          <Link
+            to="register"
+            className="btn btn-outline hover:bg-warning hover:text-white rounded-3xl"
+          >
+            I'm a Landlord
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AuthPage;
