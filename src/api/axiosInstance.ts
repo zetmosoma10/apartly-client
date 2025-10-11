078ffe9c0732f9e14077498c2e83e2f8264850e8
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-10-11 11:46:38 +0200

    add token to authentication request header

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,9 +1,20 @@
 import axios from "axios";
+import useAuthStore from "../store";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
 const api = axios.create({
   baseURL: baseUrl,
+});
+
+api.interceptors.request.use((config) => {
+  const { token } = useAuthStore.getState();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
