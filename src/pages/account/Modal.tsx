3adb1aadde814fd5471a1a3b39f6f9f2e20aad75
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-10-22 16:02:41 +0200

    manage form data with react-hook-form

--- src/pages/account/Modal.tsx
+++ src/pages/account/Modal.tsx
@@ -1,4 +1,13 @@
-import type React from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import loginSchema from "../auth/login/loginSchema";
+import Input from "../../components/Input";
+
+const passwordSchema = loginSchema.pick({ password: true });
+
+type FormData = z.infer<typeof passwordSchema>;
 
 type Props = {
   ref: React.RefObject<HTMLDialogElement | null>;
@@ -6,27 +15,44 @@ type Props = {
 };
 
 const Modal = ({ onClose, ref }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(passwordSchema) });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <dialog
       ref={ref}
       id="my_modal_5"
       className="modal modal-bottom sm:modal-middle"
     >
-      <form className="modal-box">
-        <h3 className="text-lg font-bold text-error">Confirm Deletion</h3>
+      <form onSubmit={handleSubmit(onSubmit)} className="modal-box">
+        <h3 className="text-lg font-bold">Confirm Deletion</h3>
         <p className="py-4">
           Are you sure you want to delete the account ? This action cannot be
           undone.
         </p>
 
+        <Input
+          id="password"
+          label="Password"
+          type="password"
+          placeholder="Enter Password"
+          autoFocus={true}
+          register={register("password")}
+          error={errors.password?.message}
+        />
+
         <div className="modal-action">
           <button type="button" className="btn" onClick={onClose}>
             Close
           </button>
-          <button
-            type="submit"
-            className="btn bg-error text-white hover:border-error"
-          >
+          <button type="submit" className="btn btn-neutral">
             Delete
           </button>
         </div>
