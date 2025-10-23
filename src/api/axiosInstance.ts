2cd8b51a34b196d060d4864f6de6c0c0f16938ff
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-10-24 00:17:26 +0200

    handle errors in axios response interceptor

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import useAuthStore from "../store";
+import toast from "react-hot-toast";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
@@ -16,5 +17,34 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    // !Network or connection error
+    if (!error.response) {
+      toast.error("Network error. Please check your connection.");
+    }
+    // !401 unauthorized (maybe token expired)
+    else if (error.response.status === 401) {
+      toast.error("Session expired. Please log in again.");
+
+      //  !clear token or redirect to login
+      const { clearAuth } = useAuthStore.getState();
+      clearAuth();
+    }
+    // ! 404 not found
+    else if (error.response.status === 404) {
+      toast.error("Resource not found.");
+    }
+    // ! 500 internal server
+    else if (error.response.status >= 500) {
+      toast.error("Server error. Please try again later.");
+    }
+
+    // ! Re-throw to let React Query handle it too
+    return Promise.reject(error);
+  }
+);
 
 export default api;
